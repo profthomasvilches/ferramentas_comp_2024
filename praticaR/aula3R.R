de89179cb074c60da73fad4f16bde7947f25502d
profthomasvilches <thomas.vilches@unesp.br>
2024-10-15 18:23:10 -0300

    final de ggplot e renv

--- praticaR/aula3R.R
+++ praticaR/aula3R.R
@@ -65,3 +65,129 @@ dados_hosp %>%
   theme_bw()
 
 ggsave("./praticaR/outputs/mortalidade.png", width = 12, height = 10)
+
+
+# Aula dia 15/10 -------------
+
+
+
+dados <- read.csv("praticaR/Dados/Pokemon_full.csv")
+
+glimpse(dados)
+
+
+ggplot(dados) +
+  geom_point(aes(x = weight, y = height, color = type)) +
+  geom_line(aes(x = weight, y = height, color = type)) +
+  geom_hline(aes(yintercept = 50, linetype = "TESTE"), color = "blue") +
+  guides(
+    color = guide_legend(
+      override.aes = list(linetype = NA, shape = "square", size = 3)
+    ),
+    linetype = guide_legend(
+      override.aes = list(color = "black", size = 3, linewidth = 1.5)
+    )
+  ) +
+  theme_bw()
+
+
+dados %>%
+  group_by(type) %>%
+  summarise(
+    n = n()
+  ) %>%arrange(desc(n))
+
+
+dados %>%
+  filter(grepl("^ice$|^fairy$", type)) %>%
+  ggplot() +
+  geom_point(aes(x = weight, y = height, group = type)) +
+  geom_line(aes(x = weight, y = height, group = type)) +
+  guides(
+    color = guide_legend(
+      override.aes = list(linetype = NA, shape = "square", size = 3)
+    ),
+    linetype = guide_legend(
+      override.aes = list(color = "black", size = 3, linewidth = 1.5)
+    )
+  ) +
+  theme_bw()
+
+
+
+# Aula dia 15/10 -------------
+
+m <- matrix(c(1, 2,3, 4, 5,6,7,8,9), ncol = 3)
+
+soma_ao_quadrado <- function(x){
+  sum(x^2)
+}
+
+apply(m, 1, sum)
+apply(m, 2, sum)
+apply(m, 2, function(x) sum(x^2))
+
+
+
+
+dados %>%
+  group_by(type) %>%
+  summarise(
+    n = n()
+  ) %>%arrange(desc(n))
+
+#! quero criar uma função que vai fazer o gráfico para mim
+
+t <- "fire"
+
+constroi_grafico <- function(t, df){
+  df %>%
+    filter(type == t) %>%
+    ggplot() +
+    geom_point(aes(x = weight, y = height), color = "black") +
+    geom_line(aes(x = weight, y = height), color = "black") +
+    guides(
+      color = guide_legend(
+        override.aes = list(linetype = NA, shape = "square", size = 3)
+      ),
+      linetype = guide_legend(
+        override.aes = list(color = "black", size = 3, linewidth = 1.5)
+      )
+    ) +
+    labs(title = t) +
+    theme_bw()
+}
+
+v <- dados %>%
+  pull(type) %>% unique
+
+
+# roda o lapply
+graficos <- lapply(v, constroi_grafico, df = dados)
+
+# roda o lapply para salvar os gráficos
+lapply(1:length(graficos),
+  function(x) ggsave(filename = paste0("./outputs/pokemon/grafico", x,".png"), plot = graficos[[x]], width = 5)
+)
+
+
+# roda o lapply e concatena
+lapply(c(1, 2, 3, 4), function(x) x^2) %>% 
+Reduce(rbind, .)
+
+#Reduce(lista, função que usa para juntar)
+
+constroi_sumario <- function(t, df){
+  df %>%
+    filter(type == t) %>%
+    summarise(minimo = min(height), maximo = max(height)) %>%
+    mutate(type = t)
+}
+
+# roda o lapply e junta em um data.frame
+lapply(v, constroi_sumario, df = dados) %>%
+  Reduce(rbind, .)
+
+dados %>%
+  filter(height != 38) %>%
+  constroi_sumario("fire", .)
---
+++ praticaR/renv_pratica.R
@@ -0,0 +1,8 @@
+
+
+# pacote renv
+# guardar uma foto de como os pacotes do seu computador estão
+
+# renv::init()
+# renv::snapshot() -> salva as condições atuais lockfile
+# renv::restore() -> restaura o padrão do lock file
